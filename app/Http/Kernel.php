378acc7ecff8859250647c53e07e2f5a05996aca
muthembwa <mwaniki.jacob@gmail.com>
2019-04-25 03:01:57 +0300

    add and register roleauthorization middleway

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,7 +55,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-
+        'auth.role' => \App\Http\Middleware\RoleAuthorization::class,
         'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
         'jwt.refresh' => RefreshToken::class
     ];
