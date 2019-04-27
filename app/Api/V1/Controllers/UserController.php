9e8f19d3c189b052fa71d4fbfaa72aa376d5caa9
muthembwa <mwaniki.jacob@gmail.com>
2019-04-27 09:25:40 +0300

    use user resource in user controller and add CRUD methods to access the resource

--- app/Api/V1/Controllers/UserController.php
+++ app/Api/V1/Controllers/UserController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 use App\Api\V1\Requests\LoginRequest;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use App\User;
+use App\Http\Resources\userResourse;
 use Auth;
 
 class UserController extends Controller
@@ -21,6 +23,44 @@ class UserController extends Controller
     {
         $this->middleware('auth.role:admin');
     }
+    
+    /**Show all users as in the userResourse */
+    public function index()
+    {
+      return userResourse::collection(User::with('roles')->paginate(25));
+    }
+    
+    /**store all users as in the userResourse */
+    public function store(Request $request)
+    {
+      $user = User::create([
+        'role_id' => $request->role()->id,
+        'title' => $request->title,
+      ]);
+      //$roles = \App\role::get()->pluck('title', 'id')->prepend(trans('please Select'), '');
+      return new userResourse($user);
+    }
+    public function show(User $user)
+    {
+      return new userResourse($user);
+    }
 
-   
+    public function update(Request $request, User $user)
+    {
+      // check if currently authenticated user is the owner of the user
+      if ($request->role()->id !== $user->role_id) {
+        return response()->json(['error' => 'You can not edit users own users.'], 403);
+      }
+
+      $user->update($request->only(['name', 'email', 'password','school']));
+
+      return new userResourse($user);
+    }
+
+    public function destroy(User $user)
+    {
+      $user->delete();
+
+      return response()->json(null, 204);
+    }
 }
