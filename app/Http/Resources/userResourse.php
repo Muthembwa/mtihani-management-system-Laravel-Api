ff8db499593fb866e94cfe6b5ce3169b84d45413
muthembwa <mwaniki.jacob@gmail.com>
2019-04-27 09:19:03 +0300

    add a User resource to help admins create users

---
+++ app/Http/Resources/userResourse.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class userResourse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password,
+            'school' => $this->school,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'role' => $this->role, 
+        ];
+            
+    }
+}
