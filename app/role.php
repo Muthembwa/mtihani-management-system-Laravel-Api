c3061074ee6a72a891f96b04c11f88a8a35cb118
muthembwa <mwaniki.jacob@gmail.com>
2019-05-12 08:32:10 +0300

    Commenting the role model

--- app/role.php
+++ app/role.php
@@ -6,14 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class role extends Model
 {
-   // 
-    protected $fillable = ['title'];
-    protected $hidden = [];
+   
+  use SoftDeletes;
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+  protected $fillable = ['title'];
 
-    public function users()
-    {
-      return $this
-        ->belongsToMany(User::class)
-        ->withTimestamps();
-    }
+
+     /**
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
+  protected $hidden = [];
+
+  
+  //users relationship
+  public function users()
+  {
+    return $this
+      ->belongsToMany(User::class)
+      ->withTimestamps();
+  }
 }
