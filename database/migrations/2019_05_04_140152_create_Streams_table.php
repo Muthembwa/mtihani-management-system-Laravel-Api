f9b99782277f9c41fda49e332428422ecee4570f
muthembwa <mwaniki.jacob@gmail.com>
2019-05-12 11:14:07 +0300

    Add stream model and table migration file

---
+++ database/migrations/2019_05_04_140152_create_Streams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStreamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(! Schema::hasTable('streams')) {
+            Schema::create('streams', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('class_name')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
+                $table->index(['deleted_at']);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('streams');
+    }
+}
