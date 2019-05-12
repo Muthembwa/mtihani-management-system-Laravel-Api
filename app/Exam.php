8c02df9b500f0937b8ca4e55700d456174af4817
muthembwa <mwaniki.jacob@gmail.com>
2019-05-12 11:07:21 +0300

    Add exam model and table migration file

---
+++ app/Exam.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exam extends Model
+{
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['Year', 'term', 'exam_code'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    //schools relationship
+
+    public function schools() 
+    {
+        return $this
+        ->belongsToMany(school::class)
+        ->as('schools')
+        ->withTimestamps()
+        ->withTrashed(); 
+    }
+
+
+    //streams relationship
+
+    public function stream()
+    {
+        return $this
+        ->belongsToMany(Stream::class)
+        ->as('streams')
+        ->withTimestamps()
+        ->withTrashed(); 
+    }
+
+    //users relationship
+
+    public function users() 
+    {
+        return $this
+        ->belongsToMany(User::class)
+        ->as('users')
+        ->withTrashed(); 
+    }
+
+    //students relationship
+
+    public function students() 
+    {
+        return $this
+        ->belongsToMany(Student::class)
+        ->as('students')
+        ->withTimestamps()
+        ->withTrashed(); 
+    }
+    //marks relationship
+
+    public function marks() 
+    {
+        return $this
+        ->hasMany(Mark::class)
+        ->withPivot('mark')
+        ->withTimestamps()
+        ->withTrashed(); 
+    }
+
+    
+}
---
+++ database/migrations/2019_05_04_140135_create_exams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (! Schema::hasTable('exams')){
+            Schema::create('exams', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->smallint('Year');
+                $table->integer('term');
+                $table->integer('exam_code')->nullable()->unsigned();
+                $table->timestamps();
+                $table->softDeletes();
+                $table->index(['deleted_at']);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+}
