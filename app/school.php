dedfb325dc0c581889004308a128241932eaeda4
muthembwa <mwaniki.jacob@gmail.com>
2019-05-12 09:04:02 +0300

    Add school model relationships to streams,subjects, exams and students,

--- app/school.php
+++ app/school.php
@@ -6,5 +6,69 @@ use Illuminate\Database\Eloquent\Model;
 
 class school extends Model
 {
-    //
+    use SoftDeletes;
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+   protected $fillable=['schoolname'];
+   
+     /**
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
+   protected $hidden = [];
+
+
+    //users relationship
+   public function users()
+   {
+       return $this
+       ->hasMany(User::class)
+       ->withTimestamps()
+       ->withTrashed(); 
+   }
+
+   //streams relationship
+   public function streams()
+   {
+       return $this
+       ->hasMany(Stream::class)
+       ->withTimestamps()
+       ->withTrashed(); 
+   }
+
+   //subjects relationship
+   public function subjects()
+   {
+       return $this
+       ->belongsToMany(Subject::class)
+       ->as ('subjects')
+       ->withTimestamps()
+       ->withTrashed(); 
+   }
+
+   //exams relationship
+   public function exams()
+   {
+       return $this
+       ->belongsToMany(Exam::class)
+       ->as('exams')
+       ->withTimestamps()
+       ->withTrashed(); 
+   }
+
+   //students in a school 
+   public function students()
+   {
+       return $this->hasManyThrough('App\Student', 'App\Stream');
+   }
+   //marks for the school through exams
+   public function marks()
+   {
+       return $this->hasManyThrough('App\Mark', 'App\Exam');
+   }
+
 }
