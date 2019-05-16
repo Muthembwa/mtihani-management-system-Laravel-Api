b04b4d80124fad304936fc484d15db24a34236c0
muthembwa <mwaniki.jacob@gmail.com>
2019-05-16 04:04:03 +0300

    Added resources for exams, marks, schools, subjects, students and subjects

---
+++ app/Http/Resources/streamResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class streamResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'class_name' => $this->class_name,
+            'class_slogan' => $this->class_slogan,
+            'student' => studentResource::collection($this->students),
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+        ];
+        
+    }
+}
