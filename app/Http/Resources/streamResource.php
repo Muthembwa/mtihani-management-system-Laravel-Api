21fb02b60bb765811fe911d1c2d96307c95a9b98
muthembwa <mwaniki.jacob@gmail.com>
2019-05-19 19:02:18 +0300

    Deactivate student and class slogan temporary for testing the resources

--- app/Http/Resources/streamResource.php
+++ app/Http/Resources/streamResource.php
@@ -17,8 +17,8 @@ class streamResource extends JsonResource
         return [
             'id' => $this->id,
             'class_name' => $this->class_name,
-            'class_slogan' => $this->class_slogan,
-            'student' => studentResource::collection($this->students),
+            //'class_slogan' => $this->class_slogan,
+            //'student' => studentResource::collection($this->students),
             'created_at' => (string) $this->created_at,
             'updated_at' => (string) $this->updated_at,
         ];
