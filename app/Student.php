10294d90db4b63c02ece3cffdd67d0826d9210bb
muthembwa <mwaniki.jacob@gmail.com>
2019-05-19 18:55:11 +0300

    comment soft delete

--- app/Student.php
+++ app/Student.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
 
-    use SoftDeletes;
+   // use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
