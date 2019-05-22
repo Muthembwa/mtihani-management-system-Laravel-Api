8351315cb584c3ae422e4ab25e1dccfe918c8b7a
muthembwa <mwaniki.jacob@gmail.com>
2019-05-22 07:24:50 +0300

    Comment subject controller

--- app/Subject.php
+++ app/Subject.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model
 {
     
-    use SoftDeletes;
+    //use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
